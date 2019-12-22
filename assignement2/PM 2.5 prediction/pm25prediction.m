da2c939fcfba846fb52041ffa09e4752e199f0fe
Jochem Hoes <r0666420@stud-c7-25.esat.kuleuven.be>
2019-12-23 00:14:52 +0100

    pm25

--- assignement2/PM 2.5 prediction/pm25prediction.m
+++ assignement2/PM 2.5 prediction/pm25prediction.m
@@ -1,32 +1,43 @@
 load('shanghai2017.mat');
 preprocessing
+%%
 
-iteration = 10;
-epochs = 5:5:25;
+iteration = 7;
+epstep = 5; epochs = 5:epstep:25;
 lags = 25:5:100;
 neurons = 10:10:50;
-layers = 1:1:3;
+layers = 1:1:4;
 
 errors = zeros(length(epochs), length(lags), length(neurons), length(layers));
 times = zeros(length(epochs), length(lags), length(neurons), length(layers));
 
 test = con2seq(Xpred);
 
+Xval = Xtrain(490:700); 
+Xtrain = Xtrain(1:490);
+
 for it = 1:iteration
     for i = 1:length(layers)
         for j = 1:length(neurons)
             for k = 1:length(lags)
                 [Xtr, Ytr] = getTimeSeriesTrainData(Xtrain, lags(k));
+                topology = ones(1,layers(i))*neurons(j);
+                alg = 'trainlm';
+                net = feedforwardnet(topology, alg);
+                net.trainParam.epochs = epochs(1);
+                
                 for l = 1:length(epochs)
-                    topology = ones(1,layers(i))*neurons(j);
-                    alg = 'trainlm';
-                    epoch = epochs(l);
-                    [net, time] = trainModel(topology, alg, epoch, con2seq(Xtr),con2seq(Ytr));
-                    [err, ~] = evalModel(net, Xtr(:,end), Xpred);
+                    net.trainParam.showWindow = 0;
+                    net = train(net, con2seq(Xtr),con2seq(Ytr));
+                    
+                    [err, ~] = evalModel(net, Xtr(:,end), Xval);
 
-                    times(l,k,j,i) = (times(l,k,j,i) * (it-1) + time) / it;
+%                     times(l,k,j,i) = (times(l,k,j,i) * (it-1) + time) / it;
                     errors(l,k,j,i) = (errors(l,k,j,i) * (it-1) + err) / it;
-                    fprintf('%d = The MSE of lag %d and neurons %d, layers %d, epoch %d is %f \n', it, lags(k), neurons(j), layers(i), epoch, err);
+                    fprintf('%d = The MSE of lag %d and neurons %d, layers %d, epoch %d is %f \n', it, lags(k), neurons(j), layers(i), epochs(l), err);
+                    save('backup', 'errors');
+                    
+                    net.trainParam.epochs = epstep;
                 end
             end
         end
